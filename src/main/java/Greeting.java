c87a5db57483f58ed5b3ababf50d37d29f9fcd83
jdev <saturn900@gmail.com>
2017-02-26 08:29:07 +0700

    добавление Greetings

---
+++ src/main/java/Greeting.java
@@ -0,0 +1,8 @@
+/**
+ * Created by jdev on 26.02.2017.
+ */
+public class Greeting {
+    public static void main(String... args){
+        System.out.println("Привет слушателям курса \"Профессиональная разработка на JAVA!!!!\"");
+    }
+}
