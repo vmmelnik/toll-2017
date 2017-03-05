10810cdac05ed51c04ebb771a6809794efbdbcf6
jdev <saturn900@gmail.com>
2017-03-05 15:36:57 +0700

    безжалостное удаление SRC

--- src/main/java/Greeting.java
+++
@@ -1,8 +0,0 @@
-/**
- * Created by jdev on 26.02.2017.
- */
-public class Greeting {
-    public static void main(String... args){
-        System.out.println("Привет слушателям курса \"Профессиональная разработка на JAVA!!!!\"");
-    }
-}
